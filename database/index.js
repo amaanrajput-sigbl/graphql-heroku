ddfa25e673a14358500afc3ee5859ad6654fe52e
amaan.rajput@sigbl.com <amaanrajput940@gmail.com>
2021-03-19 11:12:18 +0500

    deploye

--- database/index.js
+++ database/index.js
@@ -13,8 +13,9 @@ const connectToDB = async () => {
 
   const MONGO_URL = process.env.MONGO_URL || 'localhost:27017';
   const DB_NAME = process.env.MONGO_DB_NAME || 'sample-db';
-  const MONGO_CONNECTION_STRING = `mongodb://${mongoUserCredentials}${MONGO_URL}/${DB_NAME}`;
-
+  // const MONGO_CONNECTION_STRING = `mongodb://${mongoUserCredentials}${MONGO_URL}/${DB_NAME}`;
+  // const MONGO_CONNECTION_STRING = `mongodb+srv://amaanrajput940:vaisAOSuFLYAY5Do@gql-chatapp.tup50.mongodb.net/gql-chatapp?retryWrites=true&w=majority`;
+    const MONGO_CONNECTION_STRING = `mongodb://amaanrajput940:vaisAOSuFLYAY5Do@gql-chatapp-shard-00-00.tup50.mongodb.net:27017,gql-chatapp-shard-00-01.tup50.mongodb.net:27017,gql-chatapp-shard-00-02.tup50.mongodb.net:27017/gql-chatapp?ssl=true&replicaSet=atlas-hzt02s-shard-0&authSource=admin&retryWrites=true&w=majority`;
   await mongoose.connect(MONGO_CONNECTION_STRING);
   connection = mongoose;
 };
