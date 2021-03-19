5640c48cdebbcd331a2f82d4902a808c1d2e9278
amaan.rajput@sigbl.com <amaanrajput940@gmail.com>
2021-03-19 14:54:42 +0500

    playground

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import bodyParser from 'body-parser';
 import graphqlHTTP  from "express-graphql";
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
-const expressPlayground = require("graphql-playground-middleware-express").default;
+import expressPlayground from "graphql-playground-middleware-express";
 
 import { connectToDB } from './database';
 
